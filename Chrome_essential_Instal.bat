dffe29b954adbe22a4cf42f83f54fbe2469f10b3
BisucitKrueger <98472229+BisucitKrueger@users.noreply.github.com>
2023-06-21 03:03:01 -0400

    Add files via upload

---
+++ Chrome_essential_Instal.bat
@@ -0,0 +1,13 @@
+:made by Rat_Lord
+winget install Google.Chrome
+winget install Valve.Steam
+winget install Spotify
+winget install 7zip.7zip
+winget install vscode
+winget install Discord.Discord
+winget install hwinfo
+winget install Intel.IntelDriverAndSupportAssistant
+echo press Space to update Close Command Prompt To Stop!!
+echo press Space to update Close Command Prompt To Stop!!
+pause
+winget update --all
